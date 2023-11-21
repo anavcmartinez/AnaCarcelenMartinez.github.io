6023ec49178d48f0728a80f9a247e79157d07534
anavcmartinez <ana.carcelenmartinez@dcmail.ca>
2023-11-21 14:45:36 -0500

    New variables
    Newstory variable and randomized x, y, and z variablde

--- Lab4/Part1/main.js
+++ Lab4/Part1/main.js
@@ -14,13 +14,25 @@ function randomValueFromArray(array){
 const sotryText: "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."; 
 const insertX: ["Willy the Goblin", "Big Daddy", "Father Christmas" ];
 const insertY: ["the soup kitchen", "Disneyland", "the White House"];
-const insertX: ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertZ: ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 //Event Listener
 
 randomize.addEventListener('click', result);
 
 function result() {
+
+    // Add new variable newStory to modify original text
+    let newStory = storyText;  
+
+    //Random values from arrays insertx, inserty, insertz
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    
+
+
 
   if(customName.value !== '') {
     const name = customName.value;
