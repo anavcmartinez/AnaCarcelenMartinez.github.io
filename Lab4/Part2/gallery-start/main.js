54a20cc648306053843e574ddb8c33c67aab935e
anavcmartinez <ana.carcelenmartinez@dcmail.ca>
2023-11-22 07:18:21 -0500

    Loop and event listener
    Added a loop that creates a new img value and replaces the placeholder values
    Add event listener for click

--- Lab4/Part2/gallery-start/main.js
+++ Lab4/Part2/gallery-start/main.js
@@ -7,6 +7,9 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
+
 const alternativeText = {
     "pic1.jpg" : "A blue human eye zoomed in.",
     "pic2.jpg" : "Marble with light-brown, blue, and white patterns.",
@@ -15,13 +18,23 @@ const alternativeText = {
     "pic5.jpg" : "Brown and white moth resting on a large leaf."
 }
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
+/* Loop and create a new img element, set its src and alt attributes to a placeholder value, 
+append new img element inside thumbBar */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i=1; i<=5; i++) {
+    //create new img element
+    const newImage = document.createElement('img');
+    //Replace src and alt placeholder values
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    newImage/setAttribute('alt', alternativeText[i-1]);
+    //Append new img inside thumbBar
+    thumbBar.appendChild(newImage);
+    //Add a click event listener
+    newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
