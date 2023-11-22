35ef24b01e191606a38db70ab2d60817fe8f7c5d
anavcmartinez <ana.carcelenmartinez@dcmail.ca>
2023-11-22 06:25:26 -0500

    Constants
    Declared constants for image names, and their alternative text

--- Lab4/Part2/gallery-start/main.js
+++ Lab4/Part2/gallery-start/main.js
@@ -6,6 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const alternativeText = {
+    "pic1.jpg" : "A blue human eye zoomed in.",
+    "pic2.jpg" : "Marble with light-brown, blue, and white patterns.",
+    "pic3.jpg" : "White and purple flowers zoomed in.",
+    "pic4.jpg" : "Egyptian drawings portraying history.",
+    "pic5.jpg" : "Brown and white moth resting on a large leaf."
+}
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
