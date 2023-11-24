cc9fd0eeecc43ac989d8fa91b73020f28cdcb8dd
anavcmartinez <ana.carcelenmartinez@dcmail.ca>
2023-11-24 12:04:03 -0500

    Collision detect
    Replaced the function code with new code provided

--- Lab4/Part4/main-finished.js
+++ Lab4/Part4/main-finished.js
@@ -71,18 +71,18 @@ class Ball extends Shape {
    }
 
    collisionDetect() {
-      for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-         }
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
       }
-   }
+    }
+  }
 
 }
 
